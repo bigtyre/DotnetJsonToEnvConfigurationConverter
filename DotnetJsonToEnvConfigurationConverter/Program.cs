03b500c8426b2f0218d1c061918e07447170b0cf
Isaac <isaac@bigtyre.com.au>
2024-10-18 11:22:10 +1000

    Add project files.

---
+++ DotnetJsonToEnvConfigurationConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile(Path.Combine(AppContext.BaseDirectory, "appsettings.json"), optional: false, reloadOnChange: false);
+
+var configuration = builder.Build();
+
+static void PrintConfiguration(IConfiguration configuration, string prefix = "")
+{
+    foreach (var section in configuration.GetChildren())
+    {
+        var key = string.IsNullOrEmpty(prefix) ? section.Key : $"{prefix}:{section.Key}";
+        if (section.GetChildren().Any())
+        {
+            PrintConfiguration(section, key);
+        }
+        else
+        {
+            var envKey = key.Replace(":", "__").ToUpper();
+            Console.WriteLine($"{envKey}={section.Value}");
+        }
+    }
+}
+
+PrintConfiguration(configuration);
